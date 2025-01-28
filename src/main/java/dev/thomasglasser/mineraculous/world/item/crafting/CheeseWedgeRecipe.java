84eddb2394f0442b61541eb181582075c637cc3a
Thomas Glasser <thomas@thomasglasser.dev>
2025-01-27 19:39:28 -0500

    Fixed and expanded CheeseWedgeRecipe Updated TommyLib Removed unused tabs parameter and added properties method to MineraculousBlocks Reduced stack sizes of cheese blocks to 4 and cheese wedges to 16

--- src/main/java/dev/thomasglasser/mineraculous/world/item/crafting/CheeseWedgeRecipe.java
+++ src/main/java/dev/thomasglasser/mineraculous/world/item/crafting/CheeseWedgeRecipe.java
@@ -1,6 +1,7 @@
 package dev.thomasglasser.mineraculous.world.item.crafting;
 
 import dev.thomasglasser.mineraculous.world.level.block.CheeseBlock;
+import dev.thomasglasser.tommylib.api.world.item.BlockStateItem;
 import java.util.List;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.component.DataComponents;
@@ -12,6 +13,8 @@ import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.Property;
 
 public class CheeseWedgeRecipe extends CustomRecipe {
     public CheeseWedgeRecipe(CraftingBookCategory category) {
@@ -21,23 +24,54 @@ public class CheeseWedgeRecipe extends CustomRecipe {
     @Override
     public boolean matches(CraftingInput input, Level level) {
         List<ItemStack> items = input.items().stream().filter(stack -> !stack.isEmpty()).toList();
-        if (items.size() != 1) {
-            return false;
+        if (items.size() == 1) {
+            ItemStack only = items.getFirst();
+            BlockItemStateProperties properties = only.get(DataComponents.BLOCK_STATE);
+            Integer bites = properties != null ? properties.get(CheeseBlock.BITES) : null;
+            if (bites == null && only.getItem() instanceof BlockStateItem blockStateItem)
+                bites = blockStateItem.getState().getValue(CheeseBlock.BITES);
+            return only.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof CheeseBlock cheeseBlock && !cheeseBlock.isWaxed() && (bites == null || bites < CheeseBlock.MAX_BITES);
         } else {
-            return items.getFirst().getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof CheeseBlock cheeseBlock && !cheeseBlock.isWaxed();
+            ItemStack first = items.getFirst();
+            return items.stream().allMatch(stack -> {
+                if (stack.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof CheeseBlock cheeseBlock && !cheeseBlock.isWaxed() && stack.is(first.getItem())) {
+                    BlockItemStateProperties properties = stack.get(DataComponents.BLOCK_STATE);
+                    Integer bites = properties != null ? properties.get(CheeseBlock.BITES) : null;
+                    if (bites == null && stack.getItem() instanceof BlockStateItem blockStateItem)
+                        bites = blockStateItem.getState().getValue(CheeseBlock.BITES);
+                    return bites != null && bites >= CheeseBlock.MAX_BITES;
+                }
+                return false;
+            });
         }
     }
 
     @Override
     public ItemStack assemble(CraftingInput input, HolderLookup.Provider registries) {
-        ItemStack itemStack = input.items().stream().filter(stack -> !stack.isEmpty()).toList().getFirst();
-        BlockItemStateProperties properties = itemStack.get(DataComponents.BLOCK_STATE);
-        if (itemStack.getItem() instanceof BlockItem blockItem && blockItem.getBlock() instanceof CheeseBlock cheeseBlock && !cheeseBlock.isWaxed()) {
-            Integer bites = null;
-            if (properties != null)
-                bites = properties.get(CheeseBlock.BITES);
+        List<ItemStack> items = input.items().stream().filter(stack -> !stack.isEmpty()).toList();
+        if (items.size() == 1) {
+            ItemStack only = items.getFirst();
+            BlockItemStateProperties properties = only.get(DataComponents.BLOCK_STATE);
+            Integer bites = properties != null ? properties.get(CheeseBlock.BITES) : 0;
             if (bites == null) bites = 0;
-            return new ItemStack(cheeseBlock.getWedge().get(), 4 - bites);
+            return new ItemStack(((CheeseBlock) ((BlockItem) only.getItem()).getBlock()).getWedge().get(), 4 - bites);
+        } else {
+            CheeseBlock cheeseBlock = ((BlockItem) items.getFirst().getItem()).getBlock() instanceof CheeseBlock block ? block : null;
+            if (cheeseBlock != null) {
+                int bites = items.size();
+                BlockState state = cheeseBlock.defaultBlockState().setValue(CheeseBlock.BITES, 4 - bites);
+                ItemStack result = cheeseBlock.asItem().getDefaultInstance();
+                result.update(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY, p_330174_ -> {
+                    for (Property<?> property : state.getProperties()) {
+                        if (state.hasProperty(property)) {
+                            p_330174_ = p_330174_.with(property, state);
+                        }
+                    }
+
+                    return p_330174_;
+                });
+                return result;
+            }
         }
         return ItemStack.EMPTY;
     }

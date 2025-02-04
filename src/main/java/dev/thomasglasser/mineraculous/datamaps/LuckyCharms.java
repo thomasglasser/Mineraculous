de7ae357d3f046f4d8621de4b73394cb5a0527a8
Thomas Glasser <thomas@thomasglasser.dev>
2025-02-03 22:16:55 -0500

    Simplified LuckyCharms codec

--- src/main/java/dev/thomasglasser/mineraculous/datamaps/LuckyCharms.java
+++ src/main/java/dev/thomasglasser/mineraculous/datamaps/LuckyCharms.java
@@ -3,7 +3,6 @@ package dev.thomasglasser.mineraculous.datamaps;
 import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.EitherCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -13,8 +12,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.storage.loot.LootTable;
 
 public record LuckyCharms(Either<ResourceKey<LootTable>, HolderSet<Item>> items) {
-    public static final Codec<LuckyCharms> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-            new EitherCodec<>(ResourceKey.codec(Registries.LOOT_TABLE), HolderSetCodec.create(Registries.ITEM, BuiltInRegistries.ITEM.holderByNameCodec(), false)).fieldOf("items").forGetter(LuckyCharms::items)).apply(instance, LuckyCharms::new));
+    public static final Codec<LuckyCharms> CODEC = new EitherCodec<>(ResourceKey.codec(Registries.LOOT_TABLE), HolderSetCodec.create(Registries.ITEM, BuiltInRegistries.ITEM.holderByNameCodec(), false)).xmap(LuckyCharms::new, LuckyCharms::items);
 
     public LuckyCharms(ResourceKey<LootTable> lootTable) {
         this(Either.left(lootTable));

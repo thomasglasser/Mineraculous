b97d16941942aa00aad305a965a63a3af459bcad
Thomas Glasser <thomas@thomasglasser.dev>
2025-02-05 23:20:55 -0500

    Made the skeleton loot table apply to all in skeleton tag

--- src/main/java/dev/thomasglasser/mineraculous/data/datamaps/MineraculousDataMapProvider.java
+++ src/main/java/dev/thomasglasser/mineraculous/data/datamaps/MineraculousDataMapProvider.java
@@ -9,6 +9,7 @@ import dev.thomasglasser.mineraculous.world.level.storage.loot.MineraculousLucky
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.entity.EntityType;
 import net.neoforged.neoforge.common.data.DataMapProvider;
 import net.neoforged.neoforge.registries.datamaps.builtin.NeoForgeDataMaps;
@@ -27,13 +28,13 @@ public class MineraculousDataMapProvider extends DataMapProvider {
                 .add(MineraculousMiraculous.CAT, new LuckyCharms(MineraculousLuckyCharmLootKeys.CAT_MIRACULOUS_LUCKY_CHARM), false)
                 .build();
         builder(MineraculousDataMaps.ENTITY_LUCKY_CHARMS)
+                .add(EntityTypeTags.SKELETONS, new LuckyCharms(MineraculousLuckyCharmLootKeys.SKELETON_LUCKY_CHARM), false)
                 .add(EntityType.BLAZE.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.BLAZE_LUCKY_CHARM), false)
                 .add(EntityType.CREEPER.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.CREEPER_LUCKY_CHARM), false)
                 .add(EntityType.ENDERMAN.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.ENDERMAN_LUCKY_CHARM), false)
 //                .add(EntityType.GUARDIAN.builtInRegistryHolder(), new LuckyCharms(MineraculousItemTags.GUARDIAN_LUCKY_CHARMS), false)
                 .add(EntityType.PIGLIN.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.PIGLIN_LUCKY_CHARM), false)
                 .add(EntityType.PILLAGER.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.PILLAGER_LUCKY_CHARM), false)
-                .add(EntityType.SKELETON.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.SKELETON_LUCKY_CHARM), false)
                 .add(EntityType.WARDEN.builtInRegistryHolder(), new LuckyCharms(MineraculousLuckyCharmLootKeys.WARDEN_LUCKY_CHARM), false)
                 .build();
 

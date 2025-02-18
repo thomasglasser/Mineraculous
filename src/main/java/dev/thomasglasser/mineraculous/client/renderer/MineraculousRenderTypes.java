d37a142ffa7a4ad98ffe70edfe94331500d66efd
Thomas Glasser <thomas@thomasglasser.dev>
2025-02-18 01:56:37 -0500

    Got lucky charm shader to its final form unless someone makes a better one

--- src/main/java/dev/thomasglasser/mineraculous/client/renderer/MineraculousRenderTypes.java
+++ src/main/java/dev/thomasglasser/mineraculous/client/renderer/MineraculousRenderTypes.java
@@ -5,8 +5,10 @@ import com.mojang.blaze3d.vertex.VertexFormat;
 import dev.thomasglasser.mineraculous.Mineraculous;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 
 public class MineraculousRenderTypes {
+    private static final ResourceLocation LUCKY_CHARM_LOCATION = Mineraculous.modLoc("textures/misc/lucky_charm.png");
     private static final RenderType LUCKY_CHARM = RenderType.create(
             "mineraculous:lucky_charm",
             DefaultVertexFormat.POSITION_TEX,
@@ -14,9 +16,10 @@ public class MineraculousRenderTypes {
             1536,
             RenderType.CompositeState.builder()
                     .setShaderState(RenderStateShard.RENDERTYPE_GLINT_SHADER)
-                    .setTextureState(new RenderStateShard.TextureStateShard(Mineraculous.modLoc("textures/misc/lucky_charm.png"), false, false))
+                    .setTextureState(new RenderStateShard.TextureStateShard(LUCKY_CHARM_LOCATION, true, false))
                     .setWriteMaskState(RenderStateShard.COLOR_WRITE)
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
+                    .setTexturingState(RenderStateShard.GLINT_TEXTURING)
                     .createCompositeState(false));
     public static final RenderType ARMOR_LUCKY_CHARM = RenderType.create(
             "armor_lucky_charm",
@@ -25,10 +28,11 @@ public class MineraculousRenderTypes {
             1536,
             RenderType.CompositeState.builder()
                     .setShaderState(RenderStateShard.RENDERTYPE_ARMOR_ENTITY_GLINT_SHADER)
-                    .setTextureState(new RenderStateShard.TextureStateShard(Mineraculous.modLoc("textures/misc/armor_lucky_charm.png"), false, false))
+                    .setTextureState(new RenderStateShard.TextureStateShard(LUCKY_CHARM_LOCATION, true, false))
                     .setWriteMaskState(RenderStateShard.COLOR_WRITE)
                     .setCullState(RenderStateShard.NO_CULL)
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
+                    .setTexturingState(RenderStateShard.ENTITY_GLINT_TEXTURING)
                     .setLayeringState(RenderStateShard.VIEW_OFFSET_Z_LAYERING)
                     .createCompositeState(false));
 
--- src/main/java/dev/thomasglasser/mineraculous/mixin/minecraft/client/renderer/entity/layers/HumanoidArmorLayerMixin.java
+++ src/main/java/dev/thomasglasser/mineraculous/mixin/minecraft/client/renderer/entity/layers/HumanoidArmorLayerMixin.java
@@ -1,16 +1,15 @@
 package dev.thomasglasser.mineraculous.mixin.minecraft.client.renderer.entity.layers;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import dev.thomasglasser.mineraculous.client.MineraculousClientUtils;
+import dev.thomasglasser.mineraculous.client.renderer.MineraculousRenderTypes;
+import dev.thomasglasser.mineraculous.core.component.MineraculousDataComponents;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.Model;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.layers.HumanoidArmorLayer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,18 +18,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HumanoidArmorLayer.class)
 public class HumanoidArmorLayerMixin {
-    @Unique
-    private ItemStack mineraculous$stack;
-
-    @Inject(method = "renderArmorPiece(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/EquipmentSlot;ILnet/minecraft/client/model/HumanoidModel;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/layers/HumanoidArmorLayer;renderGlint(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/Model;)V"))
-    private void renderArmorPiece(PoseStack poseStack, MultiBufferSource bufferSource, LivingEntity livingEntity, EquipmentSlot slot, int packedLight, HumanoidModel<?> p_model, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
-        mineraculous$stack = livingEntity.getItemBySlot(slot);
+    @Inject(method = "renderArmorPiece(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/EquipmentSlot;ILnet/minecraft/client/model/HumanoidModel;FFFFFF)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;hasFoil()Z"))
+    private void renderArmorPiece(PoseStack poseStack, MultiBufferSource bufferSource, LivingEntity livingEntity, EquipmentSlot slot, int packedLight, HumanoidModel<?> model, float limbSwing, float limbSwingAmount, float partialTick, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if (livingEntity.getItemBySlot(slot).has(MineraculousDataComponents.LUCKY_CHARM))
+            mineraculous$renderLuckyCharmGlint(poseStack, bufferSource, packedLight, model);
     }
 
-    @ModifyExpressionValue(method = "renderGlint(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;ILnet/minecraft/client/model/Model;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/MultiBufferSource;getBuffer(Lnet/minecraft/client/renderer/RenderType;)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
-    private VertexConsumer renderGlint(VertexConsumer original, @Local(argsOnly = true) MultiBufferSource bufferSource) {
-        if (mineraculous$stack != null)
-            return MineraculousClientUtils.checkLuckyCharm(original, bufferSource, mineraculous$stack, true);
-        return original;
+    @Unique
+    private void mineraculous$renderLuckyCharmGlint(PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, Model model) {
+        model.renderToBuffer(poseStack, bufferSource.getBuffer(MineraculousRenderTypes.armorLuckyCharm()), packedLight, OverlayTexture.NO_OVERLAY);
     }
 }

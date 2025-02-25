fc4023643f303f4639bd7af9d644624e70c4b5f4
Gahnospinel <gabrielrusu2712@gmail.com>
2025-02-25 16:15:52 +0200

    lucky-charm-enchantment

--- src/main/java/dev/thomasglasser/mineraculous/client/renderer/MineraculousRenderTypes.java
+++ src/main/java/dev/thomasglasser/mineraculous/client/renderer/MineraculousRenderTypes.java
@@ -1,14 +1,34 @@
 package dev.thomasglasser.mineraculous.client.renderer;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import dev.thomasglasser.mineraculous.Mineraculous;
+import net.minecraft.Util;
 import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+import org.joml.Matrix4f;
 
 public class MineraculousRenderTypes {
     private static final ResourceLocation LUCKY_CHARM_LOCATION = Mineraculous.modLoc("textures/misc/lucky_charm.png");
+
+    public static final RenderStateShard.TexturingStateShard LUCKY_CHARM_TEXTURING = new RenderStateShard.TexturingStateShard(
+            "lucky_charm_texturing", () -> setupLuckyCharmTexturing(8.0F), () -> RenderSystem.resetTextureMatrix());
+
+    public static final RenderStateShard.TexturingStateShard ENTITY_LUCKY_CHARM_TEXTURING = new RenderStateShard.TexturingStateShard(
+            "entity_lucky_charm_texturing", () -> setupLuckyCharmTexturing(0.16F), () -> RenderSystem.resetTextureMatrix());
+
+    public static void setupLuckyCharmTexturing(float scale) {
+        float speed = 1 / 100f;
+        long i = (long) ((double) Util.getMillis() * speed * 8.0);
+        float f = (float) (i % 110000L) / 110000.0F;
+        float f1 = (float) (i % 30000L) / 30000.0F;
+        Matrix4f matrix4f = new Matrix4f().translation(-f, f1, 0.0F);
+        matrix4f.rotateZ((float) (Math.PI / 18)).scale(scale);
+        RenderSystem.setTextureMatrix(matrix4f);
+    }
+
     private static final RenderType LUCKY_CHARM = RenderType.create(
             "mineraculous:lucky_charm",
             DefaultVertexFormat.POSITION_TEX,
@@ -16,10 +36,10 @@ public class MineraculousRenderTypes {
             1536,
             RenderType.CompositeState.builder()
                     .setShaderState(RenderStateShard.RENDERTYPE_GLINT_SHADER)
-                    .setTextureState(new RenderStateShard.TextureStateShard(LUCKY_CHARM_LOCATION, true, false))
+                    .setTextureState(new RenderStateShard.TextureStateShard(LUCKY_CHARM_LOCATION, false, true))
                     .setWriteMaskState(RenderStateShard.COLOR_WRITE)
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
-                    .setTexturingState(RenderStateShard.GLINT_TEXTURING)
+                    .setTexturingState(LUCKY_CHARM_TEXTURING)
                     .createCompositeState(false));
     public static final RenderType ARMOR_LUCKY_CHARM = RenderType.create(
             "mineraculous:armor_lucky_charm",
@@ -28,11 +48,11 @@ public class MineraculousRenderTypes {
             1536,
             RenderType.CompositeState.builder()
                     .setShaderState(RenderStateShard.RENDERTYPE_ARMOR_ENTITY_GLINT_SHADER)
-                    .setTextureState(new RenderStateShard.TextureStateShard(LUCKY_CHARM_LOCATION, true, false))
+                    .setTextureState(new RenderStateShard.TextureStateShard(LUCKY_CHARM_LOCATION, false, false))
                     .setWriteMaskState(RenderStateShard.COLOR_WRITE)
                     .setCullState(RenderStateShard.NO_CULL)
                     .setDepthTestState(RenderStateShard.EQUAL_DEPTH_TEST)
-                    .setTexturingState(RenderStateShard.ENTITY_GLINT_TEXTURING)
+                    .setTexturingState(ENTITY_LUCKY_CHARM_TEXTURING)
                     .setLayeringState(RenderStateShard.VIEW_OFFSET_Z_LAYERING)
                     .createCompositeState(false));
 

34146ab3cb5169eb686291403c02634b90dd25b6
Thomas Glasser <thomas@thomasglasser.dev>
2025-08-30 03:36:30 -0400

    Made kwamis full bright

--- src/main/java/dev/thomasglasser/mineraculous/impl/client/renderer/entity/KwamiRenderer.java
+++ src/main/java/dev/thomasglasser/mineraculous/impl/client/renderer/entity/KwamiRenderer.java
@@ -81,15 +81,14 @@ public class KwamiRenderer<T extends Kwami> extends DynamicGeoEntityRenderer<T> 
 
     @Override
     public void actuallyRender(PoseStack poseStack, T animatable, BakedGeoModel model, @Nullable RenderType renderType, MultiBufferSource bufferSource, @Nullable VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, int colour) {
-        boolean cube = isInCubeForm(animatable);
-        if (cube) {
+        if (isInCubeForm(animatable)) {
             int summonTicks = animatable.getSummonTicks();
             float progress = summonTicks > 0 ? (float) summonTicks / (SharedConstants.TICKS_PER_SECOND * MineraculousServerConfig.get().kwamiSummonTime.getAsInt()) : 0.5F;
             int color = animatable.getMiraculous().value().color().getValue();
             renderRays(poseStack, progress, bufferSource.getBuffer(RenderType.lightning()), color);
             renderRays(poseStack, progress, bufferSource.getBuffer(RenderType.dragonRays()), color);
         }
-        super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender, partialTick, cube ? LightTexture.FULL_BRIGHT : packedLight, packedOverlay, colour);
+        super.actuallyRender(poseStack, animatable, model, renderType, bufferSource, buffer, isReRender, partialTick, LightTexture.FULL_BRIGHT, packedOverlay, colour);
     }
 
     private static void renderRays(PoseStack poseStack, float progress, VertexConsumer buffer, int color) {

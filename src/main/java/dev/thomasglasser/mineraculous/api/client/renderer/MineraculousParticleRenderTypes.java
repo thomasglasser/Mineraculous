22b4b22af71a02343b82e330c2d02f20a7508f1b
Gahnospinel <gabriel_rusu2712@yahoo.com>
2025-09-17 15:48:17 +0300

    Fixed hand particles.

--- src/main/java/dev/thomasglasser/mineraculous/api/client/renderer/MineraculousParticleRenderTypes.java
+++ src/main/java/dev/thomasglasser/mineraculous/api/client/renderer/MineraculousParticleRenderTypes.java
@@ -1,6 +1,5 @@
 package dev.thomasglasser.mineraculous.api.client.renderer;
 
-import com.mojang.blaze3d.platform.GlConst;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.BufferBuilder;
@@ -8,6 +7,7 @@ import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexFormat;
 import dev.thomasglasser.mineraculous.impl.Mineraculous;
+import net.minecraft.client.GraphicsStatus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.ParticleRenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
@@ -16,19 +16,20 @@ import net.minecraft.client.renderer.texture.TextureManager;
 public class MineraculousParticleRenderTypes {
     public static final ParticleRenderType PARTICLE_SHEET_ADDITIVE_TRANSLUCENT = new ParticleRenderType() {
         @Override
-        public BufferBuilder begin(Tesselator tesselator, TextureManager textureManager) {
+        public BufferBuilder begin(Tesselator buffer, TextureManager textureManager) {
             Minecraft.getInstance().gameRenderer.lightTexture().turnOnLightLayer();
+            boolean fabulous = Minecraft.getInstance().options.graphicsMode().get() == GraphicsStatus.FABULOUS;
             RenderSystem.enableBlend();
+            if (fabulous)
+                RenderSystem.blendFunc(
+                        GlStateManager.SourceFactor.SRC_ALPHA.value,
+                        GlStateManager.DestFactor.ONE.value);
             RenderSystem.depthMask(false);
-            RenderSystem.blendEquation(GlConst.GL_FUNC_REVERSE_SUBTRACT);
-            RenderSystem.blendFunc(
-                    GlStateManager.SourceFactor.ONE_MINUS_CONSTANT_ALPHA.value,
-                    GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA.value);
             RenderSystem.enableCull();
             RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_PARTICLES);
             RenderSystem.enableDepthTest();
-            RenderSystem.blendEquation(GlConst.GL_FUNC_ADD);
-            return tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
+
+            return buffer.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
         }
 
         @Override

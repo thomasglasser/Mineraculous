fa2ede3e0429f288f926a753a8251e35131dcb14
Thomas Glasser <thomas@thomasglasser.dev>
2025-10-16 11:09:47 -0400

    Made kwami items unlit fullbright

--- src/main/java/dev/thomasglasser/mineraculous/impl/client/renderer/item/KwamiItemRenderer.java
+++ src/main/java/dev/thomasglasser/mineraculous/impl/client/renderer/item/KwamiItemRenderer.java
@@ -1,12 +1,20 @@
 package dev.thomasglasser.mineraculous.impl.client.renderer.item;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import dev.thomasglasser.mineraculous.api.core.component.MineraculousDataComponents;
 import dev.thomasglasser.mineraculous.api.world.miraculous.Miraculous;
 import dev.thomasglasser.mineraculous.impl.client.renderer.entity.KwamiRenderer;
 import dev.thomasglasser.mineraculous.impl.world.item.KwamiItem;
 import it.unimi.dsi.fastutil.objects.Reference2ReferenceOpenHashMap;
 import java.util.Map;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.client.NeoForgeRenderTypes;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
@@ -15,6 +23,16 @@ public class KwamiItemRenderer extends GeoItemRenderer<KwamiItem> {
 
     public KwamiItemRenderer() {
         super((GeoModel<KwamiItem>) null);
+    }
+
+    @Override
+    public void defaultRender(PoseStack poseStack, KwamiItem animatable, MultiBufferSource bufferSource, @Nullable RenderType renderType, @Nullable VertexConsumer buffer, float yaw, float partialTick, int packedLight) {
+        super.defaultRender(poseStack, animatable, bufferSource, renderType, buffer, yaw, partialTick, LightTexture.FULL_BRIGHT);
+    }
+
+    @Override
+    public @Nullable RenderType getRenderType(KwamiItem animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
+        return NeoForgeRenderTypes.getUnlitTranslucent(texture);
     }
 
     @Override

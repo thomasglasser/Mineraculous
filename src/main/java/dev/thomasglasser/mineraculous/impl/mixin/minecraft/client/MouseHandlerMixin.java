a056d17dfd8e57353a08fb7900641d14e9da3c3b
Thomas Glasser <thomas@thomasglasser.dev>
2025-10-26 00:27:23 -0400

    Fixed client head turning when spectating

---
+++ src/main/java/dev/thomasglasser/mineraculous/impl/mixin/minecraft/client/MouseHandlerMixin.java
@@ -0,0 +1,21 @@
+package dev.thomasglasser.mineraculous.impl.mixin.minecraft.client;
+
+import dev.thomasglasser.mineraculous.api.world.attachment.MineraculousAttachmentTypes;
+import dev.thomasglasser.tommylib.api.client.ClientUtils;
+import net.minecraft.client.MouseHandler;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MouseHandler.class)
+public class MouseHandlerMixin {
+    @Inject(method = "turnPlayer", at = @At("HEAD"), cancellable = true)
+    private void cancelTurnWhenSpectating(double movementTime, CallbackInfo ci) {
+        Player player = ClientUtils.getLocalPlayer();
+        if (player != null && player.getData(MineraculousAttachmentTypes.ABILITY_EFFECTS.get()).spectatingId().isPresent()) {
+            ci.cancel();
+        }
+    }
+}
